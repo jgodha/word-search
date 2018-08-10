1e0dabda22976201b1e329c3058a76d31a9a9bcf
Jitendra Godha <>
2018-08-10 09:47:12 -0400

    Failing test for a simple horizonal forward search of a 2-letter word in a 2x2 grid.

--- WordSearch.py
+++ WordSearch.py
@@ -1,3 +1,3 @@
 class WordSearch:
     def search(grid, words):
-        return "foo"
+        return ["foo"]
--- WordSearchTest.py
+++ WordSearchTest.py
@@ -4,7 +4,7 @@ from WordSearch import WordSearch
 class WordSearchTest(unittest.TestCase):
     def test_word_horizontal_forward(self):
         result = WordSearch.search([['a', 'b'], ['c', 'd']] , ["ab"])
-        self.assertEqual(result, "ab: (0,0), (1,0)")
+        self.assertEqual(result, ["ab: (0,0), (1,0)"])
 
 if __name__ == '__main__':
     unittest.main()
