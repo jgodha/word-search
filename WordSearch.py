11d147732227e9b406fa07104fe26c188b5a30bc
Jitendra Godha <>
2018-09-19 11:50:37 -0400

    Simplest of implemenation to make a basic use case work for horizonal search.

--- WordSearch.py
+++ WordSearch.py
@@ -1,3 +1,18 @@
-class WordSearch:
-    def search(grid, words):
-        return ["foo"]
+def locate_in_grid(grid, ch):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == ch:
+                return i, j
+
+
+def search(grid, word):
+    positions = []
+    for ind, ch in enumerate(word):
+        positions.append(locate_in_grid(grid, ch))
+
+    result = word + ": "
+
+    for position in positions:
+        result += "(" + str(position[0]) + "," + str(position[1]) + ")"
+
+    return result
--- WordSearchTest.py
+++ WordSearchTest.py
@@ -1,10 +1,12 @@
 import unittest
-from WordSearch import WordSearch
+from WordSearch import search
+
 
 class WordSearchTest(unittest.TestCase):
     def test_word_horizontal_forward(self):
-        result = WordSearch.search([['a', 'b'], ['c', 'd']] , ["ab"])
-        self.assertEqual(result, ["ab: (0,0), (1,0)"])
+        result = search([['a', 'b'], ['c', 'd']], "ab")
+        self.assertEqual(result, "ab: (0,0)(0,1)")
+
 
 if __name__ == '__main__':
     unittest.main()
