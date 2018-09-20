aae05b985e822513b5cc401af60eee1f6a955090
Jitendra Godha <>
2018-09-20 12:07:18 -0400

    Refactoring.

--- WordSearch.py
+++ WordSearch.py
@@ -1,3 +1,10 @@
+def find_coordinates(grid, word):
+    coordinates = []
+    for ind, ch in enumerate(word):
+        coordinates.append(locate_in_grid(grid, ch))
+    return coordinates
+
+
 def locate_in_grid(grid, ch):
     coordinates = []
     for i in range(len(grid)):
@@ -7,6 +14,21 @@ def locate_in_grid(grid, ch):
     return coordinates
 
 
+def try_coordinates(coordinates, i):
+    candidate_coordinates = []
+    current_letter_coordinate = coordinates[0][i]
+    candidate_coordinates.append(current_letter_coordinate)
+    for j in range(i + 1, len(coordinates)):
+        next_letter_coordinates = coordinates[j]
+        next_letter_coordinate = find_next_adjacent_coordinate(next_letter_coordinates, current_letter_coordinate)
+        if next_letter_coordinate is None:
+            break
+        else:
+            candidate_coordinates.append(next_letter_coordinate)
+            current_letter_coordinate = next_letter_coordinate
+    return candidate_coordinates
+
+
 def find_next_adjacent_coordinate(next_letter_coordinates, current_letter_coordinate):
     for j in range(len(next_letter_coordinates)):
         if next_letter_coordinates[j][1] == current_letter_coordinate[1] + 1:
@@ -14,27 +36,13 @@ def find_next_adjacent_coordinate(next_letter_coordinates, current_letter_coordi
 
 
 def search(grid, word):
-    coordinates = []
-    for ind, ch in enumerate(word):
-        coordinates.append(locate_in_grid(grid, ch))
+    coordinates = find_coordinates(grid, word)
 
     result = word + ": "
 
-    candidate_coordinates = []
     for i in range(len(coordinates[0])):
-        current_letter_coordinate = coordinates[0][i]
-        candidate_coordinates.append(current_letter_coordinate)
-        for j in range(i+1, len(coordinates)):
-            next_letter_coordinates = coordinates[j]
-            next_letter_coordinate = find_next_adjacent_coordinate(next_letter_coordinates, current_letter_coordinate)
-            if next_letter_coordinate is None:
-                break
-            else:
-                candidate_coordinates.append(next_letter_coordinate)
-                current_letter_coordinate = next_letter_coordinate
+        candidate_coordinates = try_coordinates(coordinates, i)
         if len(candidate_coordinates) == len(word):
             for coordinate in candidate_coordinates:
                 result += "(" + str(coordinate[0]) + "," + str(coordinate[1]) + ")"
-        else:
-            candidate_coordinates = []
     return result
--- WordSearchTest.py
+++ WordSearchTest.py
@@ -3,18 +3,20 @@ from WordSearch import search
 
 
 class WordSearchTest(unittest.TestCase):
-    def test_word_horizontal_forward_every_letter_occurs_once(self):
-        result = search([['a', 'b'], ['c', 'd']], "ab")
-        self.assertEqual("ab: (0,0)(0,1)", result)
-
-    def test_word_horizontal_forward_multiple_occurrences(self):
-        result = search([
+    grid = [
             ['z', 'y', 'x', 'w', 'v'],
             ['a', 't', 's', 'r', 'q'],
             ['p', 'b', 'a', 't', 'o'],
             ['n', 'm', 'l', 'k', 'j'],
             ['i', 'h', 'g', 'b', 'e']
-        ], "bat")
+    ]
+
+    def test_word_horizontal_forward_every_letter_occurs_once(self):
+        result = search([['a', 'b'], ['c', 'd']], "ab")
+        self.assertEqual("ab: (0,0)(0,1)", result)
+
+    def test_word_horizontal_forward_multiple_occurrences(self):
+        result = search(self.grid, "bat")
         self.assertEqual("bat: (2,1)(2,2)(2,3)", result)
 
 
