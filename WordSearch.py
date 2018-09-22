32061046dd46b72665944c1a976f7e7a8aa1a2aa
Jitendra Godha <>
2018-09-21 22:51:29 -0400

    Refactoring to reduce repetitive code.

--- WordSearch.py
+++ WordSearch.py
@@ -3,24 +3,20 @@ def search(grid, word):
 
     result = word + ": "
 
+    search_strategies = [
+        find_horizontal_forward_coordinate,
+        find_horizontal_backward_coordinate,
+        find_vertical_downward_coordinate,
+        find_vertical_upward_coordinate
+    ]
+
     for i in range(len(coordinates[0])):
         starting_letter_coordinate = coordinates[0][i]
-        candidate_coordinates = try_horizontal_forward_coordinates(coordinates, starting_letter_coordinate)
-        if len(candidate_coordinates) == len(word):
-            result += report_coordinates(candidate_coordinates)
-        else:
-            candidate_coordinates = try_horizontal_backward_coordinates(coordinates, starting_letter_coordinate)
+        for st in search_strategies:
+            candidate_coordinates = try_coordinates(coordinates, starting_letter_coordinate, st)
             if len(candidate_coordinates) == len(word):
                 result += report_coordinates(candidate_coordinates)
-            else:
-                candidate_coordinates = try_vertical_downward_coordinates(coordinates, starting_letter_coordinate)
-                if len(candidate_coordinates) == len(word):
-                    result += report_coordinates(candidate_coordinates)
-                else:
-                    candidate_coordinates = try_vertical_upward_coordinates(coordinates, starting_letter_coordinate)
-                    if len(candidate_coordinates) == len(word):
-                        result += report_coordinates(candidate_coordinates)
-
+                break
     return result
 
 
@@ -40,19 +36,11 @@ def locate_in_grid(grid, ch):
     return coordinates
 
 
-def try_horizontal_forward_coordinates(coordinates, current_letter_coordinate):
-    return try_coordinates(coordinates, current_letter_coordinate, find_horizontal_forward_coordinate)
-
-
 def find_horizontal_forward_coordinate(next_letter_coordinates, current_letter_coordinate):
     for j in range(len(next_letter_coordinates)):
         if next_letter_coordinates[j][0] == current_letter_coordinate[0] \
                 and next_letter_coordinates[j][1] == current_letter_coordinate[1] + 1:
             return next_letter_coordinates[j]
-
-
-def try_horizontal_backward_coordinates(coordinates, current_letter_coordinate):
-    return try_coordinates(coordinates, current_letter_coordinate, find_horizontal_backward_coordinate)
 
 
 def find_horizontal_backward_coordinate(next_letter_coordinates, current_letter_coordinate):
@@ -62,19 +50,11 @@ def find_horizontal_backward_coordinate(next_letter_coordinates, current_letter_
             return next_letter_coordinates[j]
 
 
-def try_vertical_downward_coordinates(coordinates, current_letter_coordinate):
-    return try_coordinates(coordinates, current_letter_coordinate, find_vertical_downward_coordinate)
-
-
 def find_vertical_downward_coordinate(next_letter_coordinates, current_letter_coordinate):
     for j in range(len(next_letter_coordinates)):
         if next_letter_coordinates[j][0] == current_letter_coordinate[0] + 1 \
                 and next_letter_coordinates[j][1] == current_letter_coordinate[1]:
             return next_letter_coordinates[j]
-
-
-def try_vertical_upward_coordinates(coordinates, current_letter_coordinate):
-    return try_coordinates(coordinates, current_letter_coordinate, find_vertical_upward_coordinate)
 
 
 def find_vertical_upward_coordinate(next_letter_coordinates, current_letter_coordinate):
